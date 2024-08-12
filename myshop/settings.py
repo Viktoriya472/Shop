6a04b415280c3d58d7d76f81fefca4de4f763a00
Viktoriya Musikhina <musihina47225@mail.ru>
2024-08-12 18:29:11 +0300

    Create app product. Create models, serializer and views

--- myshop/settings.py
+++ myshop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
